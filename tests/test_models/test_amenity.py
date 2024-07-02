4007923dd41f71f9dc619c739e9a80006f6dcb12
jerm014 <jermitts@gmail.com>
2024-07-02 09:23:32 -0500

    Upload ./tests/test_models/test_amenity.py

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Tests for Amenity"""
+import unittest
+#  from models.amenity import Amenity
+#  from models import storage_type
+
+
+class test_amenity(unittest.TestCase):
+    """Class test for Amenity"""
+
+    def setUp(self):
+        """Set up test environment"""
+        pass
+        #  self.amenity = Amenity()
+
+    def test_amenity_is_instance(self):
+        """Test if Amenity instance is created"""
+        pass
+        #  self.assertIsInstance(self.amenity, Amenity)
+
+    def test_amenity_has_name_attr(self):
+        """Test if 'name' attribute exists in Amenity instance"""
+        pass
+        #  self.assertTrue(hasattr(self.amenity, 'name'))
+
+    def test_amenity_name_initial_value(self):
+        """Test initial value of 'name' attribute"""
+        pass
+        #  if storage_type == 'db':
+        #      self.assertIsNone(self.amenity.name)
+        #  else:
+        #      self.assertEqual(self.amenity.name, "")
+
+    def test_amenity_name_type(self):
+        """Test type of 'name' attribute"""
+        pass
+        #  if storage_type == 'db':
+        #      self.assertIsNone(self.amenity.name)
+        #  else:
+        #      self.assertIsInstance(self.amenity.name, str)
