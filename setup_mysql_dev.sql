06381b6a1157d57dcd80f3a7928dae758d674a1e
jerm014 <jermitts@gmail.com>
2024-07-07 22:39:59 -0500

    Delete setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++
@@ -1,8 +0,0 @@
--- make a new database make sure you USE THE UPPERCASE SQL it's dumb.
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- make a new user with all privileges on the database.
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grant all privileges to the new user on the database.
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have SELECT privilege on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
