eb8e222ee3341fd7cc595af095223f8124cb6c07
jerm014 <jermitts@gmail.com>
2024-07-14 15:06:50 -0500

    Upload ./db_storage_1.py

---
+++ db_storage_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.engine.tmp_db_storage import DBStorage
+
+
+class DBStorage(DBStorage):
+    """Doc
+    """
+
+    def count(self, cls=None):
+        """
+        returns the count of object or specific class
+        """
+        return -1
