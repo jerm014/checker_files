a396e8832d70e3d2abfd07716c6b287bb229b93a
jerm014 <jermitts@gmail.com>
2024-07-14 15:06:22 -0500

    Upload ./db_storage_0.py

---
+++ db_storage_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.engine.tmp_db_storage import DBStorage
+
+
+class DBStorage(DBStorage):
+    """Doc
+    """
+
+    def get(self, cls, id):
+        """
+        returns the object by class and ID
+        """
+        return None
