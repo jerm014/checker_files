8000454b86c7bf551cb3b671c284e9c8555de018
jerm014 <jermitts@gmail.com>
2024-07-14 15:06:35 -0500

    Upload ./file_storage_1.py

---
+++ file_storage_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.engine.tmp_file_storage import FileStorage
+
+
+class FileStorage(FileStorage):
+    """Doc
+    """
+
+    def count(self, cls=None):
+        """
+        returns the count of object or specific class
+        """
+        return -1
